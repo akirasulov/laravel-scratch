f39150d42713f55f6363fa72a5bbb7d53359e593
Akmal Rasulov <89010611+akirasulov@users.noreply.github.com>
2021-12-15 15:51:00 +0500

    Eager load relashionships

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -5,7 +5,7 @@
         <h1>{{ $post->title }}</h1>
     </a>
     <p>
-        By <a href="#">{{ $post->user->name }}</a> in
+        By <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> in
         <a href="/categories/{{ $post->category->slug }}">
             {{ $post->category->name }}
         </a>
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Models\Post;
 use App\Models\Category;
+use App\Models\User;
 use Illuminate\Support\Facades\File;
 
 // ExPost.php
@@ -22,7 +23,7 @@ use Illuminate\Support\Facades\File;
 
 Route::get('/', function () {
     return view('posts', [
-        'posts' => Post::with('category')->get()
+        'posts' => Post::latest()->get()
     ]); 
 });
 // Post::where('slug', $post)->first();
@@ -35,5 +36,11 @@ Route::get('posts/{post:slug}', function(Post $post) {
 Route::get('categories/{category:slug}', function (Category $category) {
     return view('posts', [
         'posts' => $category->posts
+    ]);
+});
+
+Route::get('authors/{author:username}', function (User $author) {
+    return view('posts', [
+        'posts' => $author->posts
     ]);
 });
